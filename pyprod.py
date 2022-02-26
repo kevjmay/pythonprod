88b2293ab8bf7dae82c85ea93bf50eda174f4ca1
kevjmay <kevinjohnmyburgh@icloud.com>
2022-02-26 16:09:46 +0000

    Additional logic added to simple rock/paper/scissors game

--- pyprod.py
+++ pyprod.py
@@ -7,3 +7,19 @@ computer_wins = 0
 #Options
 options = ["rock", "paper", "scissors"]
 
+#Logic for if input is valid or exit
+while True:
+    user_input = input("Type Rock/Paper/Scissor or to exit press Q.\n").lower()
+    if user_input == "q":
+        break
+    if user_input not in options:
+        continue
+    
+    #Logic for computers pick
+    random_number = random.randint(0,2)
+    # 0 = rock, 1 = paper, 2 = scissors
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    #Logic to evaluate wins
+
